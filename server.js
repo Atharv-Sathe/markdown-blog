a74a1ed9689b6d310c9bb669a572b0600fa67357
TheBug <atharvsathe28704@gmail.com>
2024-11-07 17:50:20 +0530

    add article model, routes, and views for creating new articles

--- server.js
+++ server.js
@@ -1,27 +1,32 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const articleRouter = require("./routes/article");
 const app = express();
+
+mongoose.connect("mongodb://localhost/blog");
 
 app.set("view engine", "ejs");
 
 app.use("/node_modules", express.static("node_modules"));
-app.use("/articles", articleRouter);
+app.use(express.urlencoded({ extended: false }));
 
 app.get("/", (req, res) => {
   const articles = [
     {
       title: "Test Article 1",
       createdAt: new Date(),
-      description: "Test description 1"
+      description: "Test description 1",
     },
     {
       title: "Test Article 2",
       createdAt: new Date(),
-      description: "Test description 2"
+      description: "Test description 2",
     },
   ];
   res.render("articles/index", { articles: articles });
 });
+
+app.use("/articles", articleRouter); // It's important to use this below express.urlencoded
 
 app.listen(5000, () => {
   console.log("I am awake!");
