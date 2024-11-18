21b67cd2fae3746cc3f1e283dc295326095c6986
TheBug <atharvsathe28704@gmail.com>
2024-11-18 23:27:22 +0530

    add article editing and viewing functionality; include dotenv for environment variables, enhance article model with slug and sanitized HTML

--- server.js
+++ server.js
@@ -1,33 +1,29 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const articleRouter = require("./routes/article");
+const methodOverride = require("method-override");
 const app = express();
+const Article = require("./models/article");
+const dotenv = require("dotenv");
 
-mongoose.connect("mongodb://localhost/blog");
+dotenv.config();
+mongoose.connect(process.env.MONGODB_CONNECT_URI);
 
 app.set("view engine", "ejs");
 
 app.use("/node_modules", express.static("node_modules"));
 app.use(express.urlencoded({ extended: false }));
+app.use(methodOverride("_method"));
 
-app.get("/", (req, res) => {
-  const articles = [
-    {
-      title: "Test Article 1",
-      createdAt: new Date(),
-      description: "Test description 1",
-    },
-    {
-      title: "Test Article 2",
-      createdAt: new Date(),
-      description: "Test description 2",
-    },
-  ];
+app.get("/", async (req, res) => {
+  const articles = await Article.find().sort({ createdAt: "desc" });
   res.render("articles/index", { articles: articles });
 });
 
 app.use("/articles", articleRouter); // It's important to use this below express.urlencoded
 
-app.listen(5000, () => {
-  console.log("I am awake!");
+const PORT = process.env.PORT;
+
+app.listen(PORT, () => {
+  console.log(`I am awake! at ${PORT}`);
 });
